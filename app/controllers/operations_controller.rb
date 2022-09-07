9bc251d176860a9b12f04415a79be513f4ddef64
Ronald <rjskinnerc@unal.edu.co>
2022-09-06 19:35:35 -0500

    Add operations routes and controller

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,7 +1,25 @@
 class OperationsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @category = Group.find(params[:category_id])
+    @groups = current_user.groups
   end
 
-  def create; end
+  def create
+    @operation = Operation.new(name: operation_params[:name], amount: operation_params[:amount], user: current_user)
+    @operation.groups << operation_params[:groups].map { |id| Group.find(id) }
+    if @operation.save
+      flash[:notice] = 'Operation was successfully created.'
+      redirect_to category_path(@operation.groups.first)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def operation_params
+    params.require(:operation).permit(:name, :amount, groups: [])
+  end
 end
--- spec/features/new_operation_spec.rb
+++ spec/features/new_operation_spec.rb
@@ -18,8 +18,7 @@ RSpec.feature 'NewOperations', type: :feature do
   scenario 'User fills out a form to create a new operation with name, amount and a default category (mandatory).' do
     sign_in @user
     visit new_category_operation_path(@category)
-    expect(page).to have_selector("input[name=\"categories[#{@category.id}]\"]")
-    expect(page).to have_selector("input[name=\"categories[#{@category2.id}]\"]")
+    expect(page).to have_selector('input[name="operation[groups][]"]')
     fill_in 'Name', with: 'Test Operation'
     fill_in 'Amount', with: 100
     click_button 'Create Operation'
@@ -32,24 +31,23 @@ RSpec.feature 'NewOperations', type: :feature do
   scenario 'User fills out a form to create a new operation with name, amount and a second category (mandatory).' do
     sign_in @user
     visit new_category_operation_path(@category)
-    expect(page).to have_selector("input[name=\"categories[#{@category.id}]\"]")
-    expect(page).to have_selector("input[name=\"categories[#{@category2.id}]\"]")
-    check "categories[#{@category2.id}]"
-    fill_in 'Name', with: 'Test Operation'
+    expect(page).to have_selector('input[name="operation[groups][]"]')
+    check "operation_groups_#{@category2.id}"
+    fill_in 'Name', with: 'Test 2 Operation'
     fill_in 'Amount', with: 100
     click_button 'Create Operation'
     expect(page).to have_content('Operation was successfully created.')
-    expect(page).to have_content('Test Operation')
+    expect(page).to have_content('Test 2 Operation')
     expect(page).to have_content('OPERATIONS')
-    expect(@category.operations.count).to eq(1)
+    expect(@category2.operations.count).to eq(1)
   end
 
   scenario 'When the user clicks on the "Back" button (<), the user navigates to the category page.' do
     sign_in @user
     visit new_category_operation_path(@category)
     click_link '<'
-    expect(page).to have_content('Test Category')
     expect(page).to have_content('OPERATIONS')
+    expect(page).to have_current_path(category_path(@category))
   end
 
   scenario 'user visits a new operation page without authenticate and is presented with the login page' do
