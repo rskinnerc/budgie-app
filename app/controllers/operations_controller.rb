4b3fd1ae1d44dcd5dd72470e2d2dad0ea8006691
Ronald <rjskinnerc@unal.edu.co>
2022-09-08 17:20:28 -0500

    Add styles to new operation view
    - Added check when saving a new operation to verify at least one selected category
    - Added stimulus controller to avoid unchecking default category

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -2,18 +2,28 @@ class OperationsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    @category = Group.find(params[:category_id])
+    @category = current_user.groups.find(params[:category_id])
     @groups = current_user.groups
+    @operation = Operation.new
   end
 
   def create
+    @category = current_user.groups.find(params[:category_id])
+    @groups = current_user.groups
     @operation = Operation.new(name: operation_params[:name], amount: operation_params[:amount], user: current_user)
+
+    if operation_params[:groups].nil?
+      flash[:notice] = 'You must select at least one category.'
+      render :new, status: :unprocessable_entity
+      return
+    end
+
     @operation.groups << operation_params[:groups].map { |id| Group.find(id) }
     if @operation.save
       flash[:notice] = 'Operation was successfully created.'
-      redirect_to category_path(@operation.groups.first)
+      redirect_to category_path(@category)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
